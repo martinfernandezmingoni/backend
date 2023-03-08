ea932df4b198ead836fc1b32b847d86b1948d969
Martin <martinlynsa@gmail.com>
2023-03-08 10:37:00 -0300

    entrega2

--- index.js
+++ index.js
@@ -1,35 +1,11 @@
-const ProductManager = require('./class');
+const ProductManager = require('./class/ProductManager.js');
 
-const productManager = new ProductManager()
+const productManager = new ProductManager('./class/products.json')
 
-productManager.addProduct({
-  id:1,
-  title:"JUAN",
-  description:"PRODUCTO JUAN",
-price:100000,
-thumbnail:"Http//juan",
-code:"SuperJuan",
-stock:1
-})
-productManager.addProduct({
-  id:2,
-  title:"NICO",
-  description:"PRODUCTO NICO",
-price:1,
-thumbnail:"Http//NICO",
-code:"nico",
-stock:1000
-})
-productManager.addProduct({
-  id:3,
-  title:"Tincho",
-  description:"PRODUCTO Tincho",
-price:100,
-thumbnail:"Http//TINCHO",
-code:"TINCHO",
-stock:10
-})
 productManager.getProducts()
 productManager.getProductsById(1)
 productManager.deleteProduct(3)
-productManager.updateProduct(1,"CARLOS")
+productManager.updateProduct(1,{title:"Everdell"})
+productManager.addProduct(
+    {title:"papa", description:"hola", price:2500, thumbnail:"a", code:"a", stock:1
+})
